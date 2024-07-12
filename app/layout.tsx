6286fe0aa05ed0f26693b5516491cb94287919f6
Quinta <0pietroquintavalle0@gmail.com>
2024-07-12 23:49:48 +0200

    deployment issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,29 +3,29 @@ import { cn } from '@/lib/utils'
 import './globals.css'
 
 const fontHeading = Manrope({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-heading',
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-heading',
 })
 
 const fontBody = Manrope({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-body',
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-body',
 })
 
-export default function Layout({ children }) {
-  return (
-      <html lang="en">
-      <body
-          className={cn(
-              'antialiased',
-              fontHeading.variable,
-              fontBody.variable
-          )}
-      >
-      {children}
-      </body>
-      </html>
-  )
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+        <body
+            className={cn(
+                'antialiased',
+                fontHeading.variable,
+                fontBody.variable
+            )}
+        >
+        {children}
+        </body>
+        </html>
+    )
 }
